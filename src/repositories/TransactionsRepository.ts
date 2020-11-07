67b5b9c029beb89c2877c9e834697e715797d8d8
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-11-07 00:35:31 -0300

    [new] solução do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,21 +15,46 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const income = Object.values(incomes).reduce(
+      (acc, { value }) => acc + value,
+      0,
+    );
+
+    const outcomes = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+    const outcome = Object.values(outcomes).reduce(
+      (acc, { value }) => acc + value,
+      0,
+    );
+    const total = income - outcome;
+    this.balance = {
+      income,
+      outcome,
+      total,
+    };
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
-
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('Type must be income or outcome.');
+    } else if (type === 'outcome' && total < value) {
+      throw new Error('Outcome extrapolate the total.');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
